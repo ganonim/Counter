7c723e21bd46eb4e78df2f24a364165a67c0db30
protocolh <jurasherepa@gmail.com>
2024-11-01 19:55:44 +0200

    def, change variabe name, and other

--- counter.py
+++ counter.py
@@ -5,9 +5,15 @@ parser.add_argument('-c', '--count', required=True, type=int, help="count")
 parser.add_argument('-r', '--range', default=4, type=int, help="counting range")
 args = parser.parse_args()
 
-range = args.range
-count = f"{hex(args.count)[2:]:-<{range}}"
+count = args.count
+count_range = args.range
 
-if len(count) > range:
-	count = "!" * range
-print("Count:", count)
+def Counter(count, count_range):
+	hex_cou = f"{hex(count)[2:]:-<{count_range}}"
+
+	if len(hex_cou) > count_range:
+		hex_cou = "!" * count_range
+	return hex_cou
+
+hex_cou = Counter(count, count_range)
+print("Count:", hex_cou)
