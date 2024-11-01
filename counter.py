4f07a153b96ce7dc72ebdf90f401251646201a0d
protocolh <jurasherepa@gmail.com>
2024-11-01 21:59:27 +0200

    add random

--- counter.py
+++ counter.py
@@ -1,14 +1,24 @@
 import argparse
+from random import randrange
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--count', required=True, type=int, help="count")
-parser.add_argument('-r', '--range', default=4, type=int, help="counting range")
+parser = argparse.ArgumentParser(
+	prog='Counter',
+	description='''Converts numbers to hex and
+	outputs them in a specific range''',
+	epilog='Text at the bottom of help',
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-c', '--count', required=True,
+	type=int, help="count")
+parser.add_argument('-r', '--range', default=4,
+	type=int, help="counting range")
 args = parser.parse_args()
 
 count = args.count
 count_range = args.range
 
 def Counter(count, count_range):
+	if count == -1:
+		count = randrange(16**count_range)
 	hex_cou = f"{hex(count)[2:]:-<{count_range}}"
 
 	if len(hex_cou) > count_range:
